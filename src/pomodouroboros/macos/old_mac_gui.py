5ddebe3f7617710a5ece7a7b99493ff068c1472b
Glyph <code@glyph.im>
2025-03-18 00:48:07 -0700

    sparkle bundle load so xcode can actually see it
    This commit was sponsored by hacklschorsch, Jason Mills, Blaise Pabon,
    and my other patrons.  If you want to join them, you can support my
    work at https://glyph.im/patrons/.

--- src/pomodouroboros/macos/old_mac_gui.py
+++ src/pomodouroboros/macos/old_mac_gui.py
@@ -825,6 +825,18 @@ def main(reactor: IReactorTime) -> None:
     ctrl = DayEditorController.alloc().initWithClock_andDayLoader_(
         reactor, dayLoader
     )
+
+    from objc import pathForFramework, loadBundle
+    from AppKit import NSBundle
+
+    b = NSBundle.mainBundle()
+    sparklePath = pathForFramework(
+        os.path.join(b.privateFrameworksPath(), "Sparkle.framework")
+    )
+
+    sparkleNS: dict[str, object] = {}
+    loadBundle("Sparkle", sparkleNS, bundle_path=sparklePath)
+
     loaded, topLevelObjects = (
         NSNib.alloc()
         .initWithNibNamed_bundle_("GoalListWindow.nib", None)
