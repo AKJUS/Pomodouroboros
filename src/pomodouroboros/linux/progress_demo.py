0884822678a05e991d36fb9e079529b004cfd7e4
Glyph <code@glyph.im>
2025-08-05 19:16:14 -0700

    bootstrapping the linux UI file
    This commit was sponsored by Anika Huhn, Alex Scammon, Guido
    Appenzeller, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

--- src/pomodouroboros/linux/progress_demo.py
+++ src/pomodouroboros/linux/progress_demo.py
@@ -1,4 +1,6 @@
+
 from itertools import cycle
+from pathlib import Path
 
 from twisted.internet.defer import Deferred
 from twisted.internet.task import deferLater
@@ -13,9 +15,24 @@ if __name__ == "__main__":
     reactor = install()
     # Create a new application
     app = Gtk.Application(application_id="im.glyph.and.this.is.Pomodouroboros")
+    # settings = Gtk.Settings.get_default()
+    # # settings.set_property("gtk-application-prefer-dark-theme", True)
+    # print("theme", settings.get_property("gtk-theme-name"))
+    # print("dark", settings.get_property("gtk-application-prefer-dark-theme"))
+    # for i in settings.list_properties():
+    #     print(i)
+    def _on_theme_name_changed(settings, gparam):
+        print("Theme name:", settings.get_property("gtk-theme-name"))
+    Gtk.Settings.get_default().connect("notify::gtk-theme-name", _on_theme_name_changed)
+    def _on_dark_changed(settings, gparam):
+        print("dark:", settings.get_property("gtk-application-prefer-dark-theme"))
+    Gtk.Settings.get_default().connect("notify::gtk-application-prefer-dark-theme", _on_dark_changed)
 
     def makeBar(app: Gtk.Application) -> None:
         bar = MultiBar.create(app)
+        stuff = Gtk.Builder.new_from_file(str(Path(__file__).parent/"linuxlegacypom.ui"))
+        stuff.get_object("my-window").present()
+
         texts = cycle([
             "let's start with this text",
             "then move on to this text",
