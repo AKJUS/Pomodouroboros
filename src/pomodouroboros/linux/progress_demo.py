9634613e08fed2ab6a18905cda579c7b968e5e6d
Glyph <code@glyph.im>
2025-08-12 11:00:00 -0700

    a button that changes a value in a table
    This commit was sponsored by Guido Appenzeller, Brian Grohe, genehack,
    and my other patrons.  If you want to join them, you can support my
    work at https://glyph.im/patrons/.

--- src/pomodouroboros/linux/progress_demo.py
+++ src/pomodouroboros/linux/progress_demo.py
@@ -1,12 +1,46 @@
 from itertools import cycle
 from pathlib import Path
+from typing import Any, TypeVar
 
 from twisted.internet.defer import Deferred
 from twisted.internet.task import deferLater
 
 from ..common import animatePct
 from .gtk_progress_bar import MultiBar
-from .platspec import Gtk, GObject
+from .platspec import Gio, GObject, Gtk
+
+T = TypeVar("T")
+
+
+def gSimpleProp(name: str, type: type[T]) -> T:
+    gprop = GObject.Property(type=type)
+    storeName = f"_{name}"
+
+    def getter(self: object) -> T:
+        result: T = getattr(self, storeName)
+        return result
+
+    getter.__name__ = name
+
+    prop = gprop(getter)
+
+    @prop.setter
+    def setter(self: object, value: Any) -> None:
+        setattr(self, storeName, value)
+
+    # return type is a polite fiction for class scope
+    return setter  # type:ignore[return-value]
+
+
+class PomItemModel(GObject.Object):
+    __gtype_name__ = "PomItemModel"
+
+    number = gSimpleProp("number", int)
+    description = gSimpleProp("description", str)
+    start = gSimpleProp("start", str)
+    end = gSimpleProp("end", str)
+    success = gSimpleProp("success", str)
+
 
 if __name__ == "__main__":
     from twisted.internet.gireactor import install
@@ -43,12 +77,80 @@ if __name__ == "__main__":
 
     def makeBar(app: Gtk.Application) -> None:
         bar = MultiBar.create(app)
-        stuff = Gtk.Builder.new_from_file(
-            str(Path(__file__).parent / "linuxlegacypom.ui")
-        )
-        loaded: object = stuff.get_object("my-window")
+        # def pom_bind_inner() -> None:
+        #     print("pom_bind_inner!!!!!!!*!********!")
+        builder = Gtk.Builder.new()
+        # cscope = Gtk.BuilderCScope.new()
+        # scope: Gtk.Builder.BuilderScope = cscope  # type:ignore[assignment]
+        # builder.set_scope(scope)
+        # cscope.add_callback_symbol(
+        #     "pom_bind",
+        #     pom_bind_inner,  # type:ignore
+        # )
+        # c = builder.create_closure("pom_bind", Gtk. BuilderClosureFlags(0))
+        # print("created", c)
+        builder.add_from_file(str(Path(__file__).parent / "linuxlegacypom.ui"))
+        print("added")
+
+        # set up column views
+
+        itemFactory = builder.get_object("description-item-factory")
+        assert isinstance(itemFactory, Gtk.SignalListItemFactory)
+
+        def descriptionItemSetup(
+            itemFactory: Gtk.SignalListItemFactory, item: Gtk.ListItem
+        ) -> None:
+            """
+            Item setup callback: construct a Gtk.Widget and call set_child on
+            item with it.
+            """
+            print("setup")
+            label = Gtk.Label(halign=Gtk.Align.START)
+            label.set_selectable(False)
+            item.set_child(label)
+
+        itemFactory.connect("setup", descriptionItemSetup)
+
+        def descriptionItemBind(
+            itemFactory: Gtk.SignalListItemFactory, item: Gtk.ListItem
+        ) -> None:
+            print("bind")
+            widget = item.get_child()
+            assert isinstance(
+                widget, Gtk.Label
+            ), "should be set up in descriptionItemSetup"
+            itemsItem = item.get_item()
+            assert isinstance(
+                itemsItem, PomItemModel
+            ), "should be added with store.insert"
+            widget.set_label(itemsItem.description)
+            itemsItem.bind_property(
+                "description",
+                widget,
+                "label",
+                GObject.BindingFlags.SYNC_CREATE,
+            )
+
+        itemFactory.connect("bind", descriptionItemBind)
+
+        loaded: object = builder.get_object("my-window")
         assert isinstance(loaded, Gtk.Window)
         loaded.present()
+        store: object = builder.get_object("the-list-store")
+        assert isinstance(store, Gio.ListStore), store
+
+        print("creating model")
+        one = PomItemModel(description="one")
+        store.insert(0, one)
+        two = PomItemModel(description="two")
+        store.insert(0, two)
+        # store.insert(1, one)
+
+        button = builder.get_object("debug-button")
+        def setdesc(theButton: Gtk.Button)->None:
+            one.description = "three"
+        assert isinstance(button, Gtk.Button), "should be a button declared in the UI"
+        button.connect("clicked", setdesc)
 
         texts = cycle(
             [
