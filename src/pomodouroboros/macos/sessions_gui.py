fedfd8d32d1fab1ed7e5ab7ce7273add493c6da3
Glyph <code@glyph.im>
2025-12-12 15:20:10 -0800

    adjust session gui to point at session manager attributes
    This commit was sponsored by Mathieu Dupuy, Jason Mills, Guido
    Appenzeller, and my other patrons.  If you want to join them, you can
    support my work at https://glyph.im/patrons/.

--- src/pomodouroboros/macos/sessions_gui.py
+++ src/pomodouroboros/macos/sessions_gui.py
@@ -5,9 +5,9 @@ from datetype import DateTime
 from Foundation import NSObject
 from fritter.drivers.datetimes import guessLocalZone
 
-from pomodouroboros.model.util import showFailures
-
+from ..model.util import showFailures
 from ..model.nexus import Nexus
+from ..model.sessions import Session
 
 TZ = guessLocalZone()
 
@@ -22,10 +22,16 @@ class SessionDataSource(NSObject):
     def awakeWithNexus_(self, newNexus: Nexus) -> None:
         self.nexus = newNexus
 
+    def sessions(self) -> list[Session]:
+        return list(
+            *self.nexus._sessionManager.previousSessions,
+            *self.nexus._sessionManager.upcomingSessions,
+        )
+
     # pragma mark NSTableViewDataSource
 
     def numberOfRowsInTableView_(self, tableView: NSTableView) -> int:
-        return len(self.nexus._sessions)
+        return len(self.sessions())
 
     # This table is not editable.
     def tableView_shouldEditTableColumn_row_(
@@ -49,7 +55,7 @@ class SessionDataSource(NSObject):
         row: int,
     ) -> dict[str, str]:
         with showFailures():
-            session = self.nexus._sessions[row]
+            session = self.sessions()[row]
             startDT = DateTime.fromtimestamp(session.start, TZ)
             endDT = DateTime.fromtimestamp(session.end, TZ)
             return {
