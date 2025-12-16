cab4fa2b63889dfcf57bccd9174540093e86db08
Glyph <code@glyph.im>
2025-12-16 11:14:02 -0800

    failing test: still in session when we should not be
    This commit was sponsored by zts, genehack, Jason Walker, and my other
    patrons.  If you want to join them, you can support my work at
    https://glyph.im/patrons/.

--- src/pomodouroboros/model/test/test_sessions.py
+++ src/pomodouroboros/model/test/test_sessions.py
@@ -105,5 +105,55 @@ class SessionStartEndSchedulingTests(TestCase):
             observer.extraneousChanges,
         )
 
-    def observeManualSession(self) -> None:
-        pass
+    def test_observeManualSession(self) -> None:
+        scheduler: Scheduler[float, Callable[[], None], int] = (
+            schedulerFromDriver(memory := MemoryDriver())
+        )
+        desiredStart = aware(
+            datetime(2023, 11, 7, 3, 4, 5, tzinfo=PT), ZoneInfo
+        ).timestamp()
+        desiredEnd = aware(
+            datetime(2023, 11, 7, 4, 5, 6, tzinfo=PT), ZoneInfo
+        ).timestamp()
+        # start at a reasonable time that is not in the 1970s
+        memory.advance(
+            aware(datetime(2023, 11, 7, 2, tzinfo=PT), ZoneInfo).timestamp()
+            - memory.now()
+        )
+        observer = Observe()
+
+        asm = SessionManager.new(observer, scheduler, PT)
+        if 0:
+            addObserver(asm, DebugChanges())
+        asm.addManualSession(desiredStart, desiredEnd)
+        self.assertIs(asm.activeSession, None)
+        memory.advance(desiredStart + 10 - memory.now())
+        self.assertEqual(
+            asm.activeSession,
+            Session(start=desiredStart, end=desiredEnd, automatic=False),
+        )
+        memory.advance(desiredEnd + 15 - memory.now())
+        self.assertEqual(asm.activeSession, None)
+        expectedSession = Session(
+            start=desiredStart, end=desiredEnd, automatic=True
+        )
+        self.assertEqual(
+            observer.sessionChanges,
+            [
+                ("activeSession", None, expectedSession),
+                ("activeSession", expectedSession, None),
+            ],
+        )
+        expectedRule = DailySessionRule(
+            dailyStart=naive(time(3, 4, 5)),
+            dailyEnd=naive(time(4, 5, 6)),
+            days={Weekday.tuesday, Weekday.wednesday, Weekday.friday},
+        )
+        self.assertIn(
+            (
+                "add",
+                "rules",
+                [expectedRule],
+            ),
+            observer.extraneousChanges,
+        )
