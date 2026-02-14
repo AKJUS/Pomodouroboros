53848d2b94c7f9ba3eec375fecfc090c540bc09d
Glyph <code@glyph.im>
2026-02-13 17:00:46 -0800

    interactive model debugger
    invoke with
    
    python -im pomodouroboros.model.debugger
    
    This commit was sponsored by Geoffrey Thomas, Matt Campbell, James
    Harley Gorrell, and my other patrons.  If you want to join them, you
    can support my work at https://glyph.im/patrons/.

--- src/pomodouroboros/model/debugger.py
+++ src/pomodouroboros/model/debugger.py
@@ -4,3 +4,16 @@ def debug(*x: object) -> None:
     """
     if 0:
         print(*x)
+
+
+if __name__ == '__main__':
+    from pomodouroboros.model.storage import loadDefaultNexus
+    from pomodouroboros.model.test.test_model import TestUserInterface
+    from twisted.internet.task import Clock
+    from time import time
+
+    clock = Clock()
+    clock.advance(time())
+    ui = TestUserInterface(clock)
+    nexus = loadDefaultNexus(clock.seconds(), ui.setIt)
+
